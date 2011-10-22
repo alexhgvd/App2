026bdce738c4065750e31d8c71cc5cfab0707254
alex <alex@hgvd.org>
2011-10-22 18:29:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App2::Application.config.secret_token = '5053b088784e5133286128723907517dfc5384a8c9a9cb10f9f563c95168b4e4729bd8880e748ee229ea7ba192d4c1600a38877733505bedb7ce727694276782'
